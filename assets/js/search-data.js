ac95cd51304c90de503eabcae2499045e520e7b7
prebish <125402096+prebish@users.noreply.github.com>
2025-02-02 19:57:16 +0000

    Deploying to gh-pages from @ prebish/prebish.github.io@27fa0f37f737bea4fea2fda8a27e4e9fd7f75f1f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,9 +421,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          section: "News",},{id: "projects-python-projects-hub",
+          title: 'Python Projects Hub',
+          description: "free eduactional python projects for all!",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
