cf9177320cd45ca33d7bb54437facfd8977a4a3d
prebish <125402096+prebish@users.noreply.github.com>
2025-02-02 19:43:07 +0000

    Deploying to gh-pages from @ prebish/prebish.github.io@b1c496bd5ce041f6ed7ba9ebc8cf9d52a926ebed 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,9 +421,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-flipper-zero-toolbox",
+          title: 'Flipper Zero Toolbox',
+          description: "all the tools i use for my flipper zero (va4ky)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
